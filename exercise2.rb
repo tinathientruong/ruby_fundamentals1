74e9d4d6002987e334e80db80b73db831b865943
Tina Truong <tina.truong21@gmail.com>
2016-03-22 14:50:45 -0400

    completed exercise 4

--- exercise2.rb
+++ exercise2.rb
@@ -1,7 +1,9 @@
 # 1. How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
+  tip_percent = 18 
+ tip_amount = 55 * (tip_percent.to_f/100)
+ puts "This is a good tip amount #{tip_amount}" 
 
 #2. Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
-
 4.to_s 
 puts "There are a total of 4 dogs in the park"
 
@@ -10,3 +12,5 @@ puts "#{45628*7839}"
 
 #4. What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
 puts (10 < 20 && 30 < 20) || !(10 == 11)
+
+
---
+++ exercise4.rb
@@ -0,0 +1,14 @@
+(1..100).each do |x|
+	if (x%3==0) && (x%5==0) 
+		puts "Bitmaker"
+
+	elsif x % 3==0
+		puts "Bit" 
+
+	elsif x%5==0
+		puts "Maker"
+
+	else 
+		puts x 
+	end 
+end
